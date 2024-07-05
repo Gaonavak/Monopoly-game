0e69420c76ac3bd00ac79f2a8fcdd3389e1fcaf2
mn <nimingsq@outlook.com>
2024-07-06 02:15:22 +0800

    add: 安装 uview-plus 插件，新增商城页面

---
+++ components/product/product.vue
@@ -0,0 +1,63 @@
+<template>
+    <view class="product">
+        <image :src="src" mode="aspectFit" class="image"></image>
+        <view class="text">
+            <view class="title">标题</view>
+            <view class="description">好吃好吃好吃好吃好吃好吃好吃好吃好吃好吃好吃好吃</view>
+            <view class="price">999 积分</view>
+        </view>
+    </view>
+</template>
+
+<script setup>
+    import {
+        defineProps
+    } from 'vue';
+
+    const props = defineProps({
+        src: {
+            type: String,
+            required: true
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+    .product {
+        width: 100%;
+        height: 300rpx;
+        background: #fff;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        .image {
+            width: 250rpx;
+            height: 250rpx;
+            object-fit: cover;
+            margin: 10rpx;
+        }
+
+        .text {
+            width: 100%;
+            height: 100%;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+
+            .title, .description, .price{
+                flex: 1;
+                display: flex;
+                align-items: center;
+                padding-left: 10rpx;
+                padding-right: 10rpx;
+            }
+            
+            .title{
+                padding-top: 20rpx;
+                font-size: 40rpx;
+            }
+        }
+    }
+</style>
--- main.js
+++ main.js
@@ -2,6 +2,20 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import uviewPlus from 'uview-plus'
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  app.use(uviewPlus)
+  return {
+    app
+  }
+}
+// #endif
+
+
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -20,3 +34,4 @@ export function createApp() {
   }
 }
 // #endif
+
